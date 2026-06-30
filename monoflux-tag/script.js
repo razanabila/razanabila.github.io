0343f497a08aa8fa77021d3f959a56ea6671ba6c
Rendi Marzikri <arternosrendi@gmail.com>
2026-06-30 09:58:17 +0700

    Refactor Tag.create calls into single line

--- monoflux-tag/script.js
+++ monoflux-tag/script.js
@@ -1,15 +1,1 @@
-Tag.create("utama", {
-  render(html) {
-    return `<main>${html}</main>`;
-  }
-});
-Tag.create("konten", {
-  render(html) {
-    return `<div class="content">${html}</div>`;
-  }
-});
-Tag.create("kartu", {
-  render(html) {
-    return `<div class="widget">${html}</div>`;
-  }
-});
+Tag.create("utama",{render(html){return `<main>${html}</main>`;}});Tag.create("konten",{render(html){return `<div class="content">${html}</div>`;}});Tag.create("kartu",{render(html) {return `<div class="widget">${html}</div>`;}});
